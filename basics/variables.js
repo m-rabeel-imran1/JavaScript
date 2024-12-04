59227a201b809af63a47d0328725f36a8dd1a095
Muhammad Rabeel Imran <arainrabeel65@gmail.com>
2024-12-04 13:41:54 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 1223345
+
+let accountEmail = "arainrabeel65@gmail.com"
+
+var accountPassword = '098765'
+
+accountCity = "Lahore"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+accountEmail = "rabeel12@gmail.com"
+
+accountPassword = "5678"
+
+accountCity = "Karachi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
